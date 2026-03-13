ad17d7e8566db3259b2b915292283b2eb5a4dada
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-03-13 18:34:35 -0400

    reduce flake usage

--- flake.nix
+++ flake.nix
@@ -1,21 +1,11 @@
 {
   description = "A helper for building Typst document and importing non-Universe packages";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
   outputs =
     {
       self,
-      nixpkgs,
     }:
     let
-      system = "x86_64-linux";
-
       overlay = import ./.;
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ overlay ];
-      };
     in
     {
       overlays = {
@@ -23,29 +13,9 @@
         buildTypst = overlay;
       };
 
-      checks.x86_64-linux = builtins.removeAttrs (pkgs.callPackage ./tests { }) [
-        "override"
-        "overrideDerivation"
-      ];
-
       templates.default = {
         path = ./template;
         description = "A basic template using Press";
-      };
-
-      formatter.${system} = pkgs.nixfmt-tree;
-
-      devShells.${system}.default = pkgs.mkShell {
-        stdenv = pkgs.stdenvNoCC;
-        packages =
-          let
-            p = pkgs;
-          in
-          [
-            p.nil
-            p.nixfmt-tree
-            p.typstyle
-          ];
       };
     };
 }
